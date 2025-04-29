10a643afe0f6e287bcfb6de12ba1c7fedc991f81
pernillasterner <psterner@hotmail.com>
2025-04-29 21:13:07 +0200

    Add table rows for title, content, published and foreign key connected to creator of BlogPost

--- database/migrations/2025_04_29_190044_create_blog_posts_table.php
+++ database/migrations/2025_04_29_190044_create_blog_posts_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->text('content');
+
+            $table->timestamp('published_at')->nullable;
+
+            $table->foreignId('user_id')->constrained('users');
+
             $table->timestamps();
         });
     }

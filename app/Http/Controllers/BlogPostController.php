63336efb7decead32c5fc6b582ff1bd45b8772ee
pernillasterner <psterner@hotmail.com>
2025-05-01 14:49:19 +0200

    Clean up

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -6,12 +6,8 @@ use App\Models\BlogPost;
 
 class BlogPostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        // get 10 posts for each page and pass them to view blog index
         $posts = BlogPost::with('user')->paginate(10);
         return view('blog_posts.index', ['posts' => $posts]);
     }
